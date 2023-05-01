058c005bb58c17ae66a7d40514696f4bf95b454d
31000s <burakyabgu@gmail.com>
2023-05-01 18:43:08 +0300

    create: Consolidation of the foundations

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>So Site</h1>
+    </div>
+  )
+}
+
+export default App
