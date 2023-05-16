f102c695bb4fd7e5884f156575ab1d91abb23e58
31000s <burakyabgu@gmail.com>
2023-05-16 05:05:08 +0300

    add: Add Post Button

--- server/app.js
+++ server/app.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const cors = require('cors')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 
+const { MONGODB_URL } = require('./settings.json');
 const Post = require('./models/Post')
 
 
 const app = express()
 let arr = []
 
+
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors({origin: true}))
+app.use(
+    methodOverride('_method', {
+        methods: ['POST', 'GET'],
+    })
+);
+
+mongoose.connect(MONGODB_URL).then(() => {
+    console.log("Veritabanına erisim sağlandı.");
+});
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -28,17 +40,12 @@ app.get('/dashboard', (req,res)=>{
     })
 })
 
-app.post('/addPost', (req,res)=>{
-    arr.push({
-        text: req.body.text,
-        date: req.body.date,
-        url: req.body.url
-    })
-
-    res.redirect('/')
+app.post('/addPost', async (req,res)=>{
+    await Post.create(req.body)
+    res.status(200).redirect('/')
 })
 
-app.delete('deletePost', async (req,res)=>{
+app.delete('/deletePost', async (req,res)=>{
     try {
         await Post.findOneAndRemove(req.body)
         res.status(200).redirect('/')
