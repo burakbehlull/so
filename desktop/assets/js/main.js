765f5be29adb23c65eb561a98c44c565c2a656d1
31000s <burakyabgu@gmail.com>
2023-05-16 13:59:21 +0300

    feat: Data listed (Electronjs)

--- desktop/assets/js/main.js
+++ desktop/assets/js/main.js
@@ -5,3 +5,30 @@ const closeBtn = document.getElementById('close')
 closeBtn.addEventListener('click', ()=>{
     ipcRenderer.send('close')
 })
+
+fetch('http://localhost/posts')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        for(let item of data){
+            const posts = document.getElementById('posts')
+
+            let MasterTag = document.createElement('li')
+            let DateTag = document.createElement('span')
+            let LinkTag = document.createElement('a')
+
+            posts.appendChild(MasterTag)
+            DateTag.innerHTML = item.date
+            LinkTag.innerHTML = item.text
+            LinkTag.href = item.url
+            MasterTag.appendChild(DateTag)
+            MasterTag.appendChild(LinkTag)
+        }
+    })
+
+    .catch(error => {
+        console.error('Veri alma hatası:', error);
+    });
+
+
