49a87215a9aa991b3db7136096b1fcaf610c01d1
31000s <burakyabgu@gmail.com>
2023-05-16 07:11:02 +0300

    feat: Style operations and setting minimum values

--- desktop/app.js
+++ desktop/app.js
@@ -6,9 +6,11 @@ let mainWindow;
 
 app.on('ready', ()=>{
     mainWindow = new BrowserWindow({
+        frame: false,
         width: 800,
         height: 550
     })
+    mainWindow.setMinimumSize(220, 140);
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, "pages/main.html"),
         protocol: "file:"
